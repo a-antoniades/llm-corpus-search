75970cd6938b6da189afbca6214c7af2dea715b6
woanderer <antonis@ucsb.edu>
2025-01-21 22:17:53 -0800

    Initial commit with core search functionality

--- run_wimbd_rust.sh
+++
@@ -1,2 +0,0 @@
-wimbd count /share/edc/home/antonis/datasets/huggingface/tuluv2_ds.json.gz \
-            --search "cat"
--- scripts/make_dataset.sh
+++
@@ -1,4 +0,0 @@
-python src/mix_c4_flan_V1.py --task QA
-# python src/mix_c4_flan_V1.py --task Summarization
-# python src/mix_c4_flan_V1.py --task Commonsense
-# python src/mix_c4_flan_V1.py --task NLI
